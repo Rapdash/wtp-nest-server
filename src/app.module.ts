a4a3261bbef41046dce8d380a30d6567cc29ea1f
ottovbismarck <fritzj96@gmail.com>
2019-06-25 09:20:37 -0700

    added dist to gitignore && messed with logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
-import { Module } from '@nestjs/common';
+// Packages
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+
+// Middleware
+import { LoggerMiddleware } from './middleware/logger.middleware';
+
+// Modules
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('users');
+  }
+}
