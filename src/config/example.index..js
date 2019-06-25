b9599192b7d5280903d2d9c95b957fc60977ad89
ottovbismarck <fritzj96@gmail.com>
2019-06-25 10:31:24 -0700

    tslint stuff plus created a schema & changed js to ts

--- src/config/example.index..js
+++
@@ -1,1 +0,0 @@
-export const MONGO_URI = MONGO_URI = "mongodb://Garfield:Odie@ds253804.mlab.com:53804/twai-app"
---
+++ src/config/example.index.ts
@@ -0,0 +1,1 @@
+export const MONGO_URI = 'mongodb://Garfield:Odie@ds253804.mlab.com:53804/twai-app';
