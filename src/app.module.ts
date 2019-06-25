a279f1620a782dbd61abffc3346e20c2165a00d6
ottovbismarck <fritzj96@gmail.com>
2019-06-25 10:27:50 -0700

    set up config folder for Mongo URI && other stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 // Middleware (not currently using any)
 
+// Config
+import { MONGO_URI } from './config';
+
 // Modules
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGOOSE_URI), UsersModule],
+  imports: [MongooseModule.forRoot(MONGO_URI), UsersModule],
 })
 export class AppModule {}
