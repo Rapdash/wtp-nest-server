7978b8d32618f0d2924ab3446312415652952ade
ottovbismarck <fritzj96@gmail.com>
2019-06-25 10:34:35 -0700

    created UserSchema & installed mongoose types

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import { Schema } from 'mongoose';
+
+export const UserSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
