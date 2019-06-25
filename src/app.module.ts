472b6ec348fef56ee128d8371f9d08ae6afa02de
ottovbismarck <fritzj96@gmail.com>
2019-06-25 09:29:33 -0700

    removed extra imports from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 // Packages
-import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
 // Middleware (not currently using any)
 
 // Modules
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UsersModule],
